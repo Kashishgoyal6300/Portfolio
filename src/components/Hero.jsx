6a692cadf5f6b621e6024579b8f7b50c2104a3ed
Kashish Goyal <kashishgoyal0003@gmail.com>
2026-07-21 00:12:19 +0530

    id changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,7 +45,7 @@ export default function Hero() {
         <motion.div className="hero-typing" variants={item}>
           <span className="typing-prefix"></span>
           <TypingText words={typingRoles} />
-        </motion.div>
+        </motion.div>https://www.linkedin.com/in/kashish-goyal-421572267/
 
         <motion.p className="hero-tagline" variants={item}>
           {personalInfo.tagline}
